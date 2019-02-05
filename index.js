63ade925c36c4bb5a4e7dad1f50708a8ebdf9fc6
manon jacquin <manon.jacquin@gmail.com>
2019-02-05 20:05:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const play = []
+  
+  for (var i = 0; i < musicians.length; i++) {
+    play.push(musicians[i] + " plays " + instruments[i])
+  }
+  
+  return play
+
+}
+
+function johnLennonFacts(array) {
+  var exclamation = []
+  for (var i = 0; i < array.length; i++) {
+    exclamation.push(array[i] + "!!!")
+  }
+  
+  return exclamation
+ 
+}
+
+function iLoveTheBeatles(num) {
+  var i = 0
+  
+}
+
+
